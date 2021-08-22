95b25bfe363e577f7feeeaf1cc4dacd53d1c3f0c
mikolajsmigielski <mikolaj.smigielski@gmail.com>
2021-08-22 19:38:25 +0200

    Added shooting functionality

---
+++ Assets/Scripts/DelayedDestroy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelayedDestroy : MonoBehaviour
+{
+    public float time = 5f;
+    void Start()
+    {
+        Destroy(gameObject, time);
+    }
+
+    
+}
