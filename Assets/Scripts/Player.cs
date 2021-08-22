a981656131e3538cf4141fc3aa455236c4081e95
mikolajsmigielski <mikolaj.smigielski@gmail.com>
2021-08-22 14:02:24 +0200

    Added movment script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var WalkingDirection = Vector2.zero;
+
+        WalkingDirection += Vector2.up * Input.GetAxis("Vertical");
+        WalkingDirection += Vector2.right * Input.GetAxis("Horizontal");
+        WalkingDirection = WalkingDirection.normalized * speed;
+
+        var rigidbody = GetComponent<Rigidbody2D>();
+        rigidbody.velocity = WalkingDirection;
+
+        transform.right = WalkingDirection;
+    }
+}
