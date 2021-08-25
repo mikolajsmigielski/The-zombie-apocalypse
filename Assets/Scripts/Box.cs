18acf14cdb8c2867ca009d8c32d2ac94612b2ac8
mikolajsmigielski <mikolaj.smigielski@gmail.com>
2021-08-25 17:31:04 +0200

    Added loot boxes

---
+++ Assets/Scripts/Box.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Activator))]
+[RequireComponent(typeof(GenerateLoot))]
+public class Box : MonoBehaviour
+{
+
+    void Start()
+    {
+        GetComponent<Activator>().OnActivated += () =>
+        {
+            GetComponent<GenerateLoot>().Generate();
+            Destroy(gameObject);
+        };
+    }
+    
+}
---
+++ Assets/Scripts/GenerateLoot.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateLoot : MonoBehaviour
+{
+    [SerializeField]
+    GameObject Prefab;
+
+    [SerializeField]
+    int Min = 5;
+
+    [SerializeField]
+    int Max = 15;
+
+    [SerializeField]
+    float Probability = 0.25f;
+    
+    public void Generate()
+    {
+        if (Random.value > Probability)
+            return;
+        var loot = Instantiate(Prefab);
+        loot.transform.position = transform.position;
+
+        loot.GetComponent<Bullets>().Amount = Random.Range(Min, Max);
+    }
+
+}
