6edf916cca6d561cf8417ed1a6b576f13207a494
mikolajsmigielski <mikolaj.smigielski@gmail.com>
2021-08-25 13:12:57 +0200

    Created activator script

---
+++ Assets/Scripts/Activator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class Activator : MonoBehaviour
+{
+    public bool Active { get; private set; }
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        GetComponent<Renderer>().material.color = Active ? Color.red : Color.white;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Active = true;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Active = false;
+    }
+}
