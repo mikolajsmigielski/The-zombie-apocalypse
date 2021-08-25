ab70127d3850266c34abb7f6029bbffaef147f74
mikolajsmigielski <mikolaj.smigielski@gmail.com>
2021-08-25 16:07:55 +0200

    Created Camera script for movment

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    Rigidbody2D Player;
+    UnityEngine.Camera MainCamera;
+    void Start()
+    {
+        Player = FindObjectOfType<Player>().GetComponent<Rigidbody2D>();
+        MainCamera = GetComponent<UnityEngine.Camera>();
+    }
+
+    
+    void FixedUpdate()
+    {
+        UpdatePosition();
+        UpdateFOV();
+    }
+    void UpdatePosition()
+    {
+        var targetPosition = (Vector3)Player.position + (Vector3)Player.velocity * 0.75f + Vector3.back * 10f;
+
+        transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * 4f);
+    }
+    void UpdateFOV()
+    {
+        var speed = Player.velocity.magnitude;
+        var targetViewSize = 3f + speed / 2f;
+        MainCamera.orthographicSize = Mathf.Lerp(MainCamera.orthographicSize, targetViewSize, Time.deltaTime * 2f);
+    }
+}
